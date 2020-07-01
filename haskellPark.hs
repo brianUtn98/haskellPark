a2c35d60c9cc4d2b2a251c3f1e465dcad63ac84b
brianUtn98 <brian.gmonroy98@gmail.com>
2020-07-01 17:45:36 -0300

    Punto 2

--- haskellPark.hs
+++ haskellPark.hs
@@ -17,3 +17,24 @@ ana = Persona "Ana" 20 10 60
 
 juan::Persona
 juan = Persona "Juan" 30 20 40
+
+--Punto 2 Modelado de las atracciones
+
+type Atraccion = Persona->Persona --Se modela el sinónimo de tipo atracción.
+
+montanaRusa::Float->Float->Atraccion
+montanaRusa velocidad altura persona
+  | velocidad > 50 = persona {nivelEmocion = nivelEmocion persona + 0.15*velocidad + altura}
+  | otherwise = persona {nivelEmocion = nivelEmocion persona * 0.95,nivelSatisfaccion = nivelSatisfaccion persona * 0.9}
+
+caidaLibre::Float->Atraccion
+caidaLibre altura persona = persona {nivelEmocion = nivelEmocion persona + 0.2*altura}
+
+mundoMaya::Atraccion
+mundoMaya persona = persona {nivelEmocion = nivelEmocion persona * 1.10,nivelCultura = nivelCultura persona * 1.20}
+
+
+showDeMagia::Atraccion
+showDeMagia persona 
+  | nivelCultura persona > 50 = persona {nivelSatisfaccion = nivelSatisfaccion persona + 20}
+  | otherwise = persona {nivelEmocion = nivelEmocion persona + 30}
